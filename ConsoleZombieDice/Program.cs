8f632959ff9002391a611e470ecb9a80bb94013c
Aaron <aaron.langille@gmail.com>
2026-09-24 10:41:37 -0400

    More functionality.

--- ConsoleZombieDice/Program.cs
+++ ConsoleZombieDice/Program.cs
@@ -22,36 +22,110 @@ namespace ConsoleZombieDice
 
         static void Main(string[] args)
         {
-            List<Die> dice = new List<Die>
-            {
-                new GreenDie(),
-                new GreenDie(),
-                new GreenDie(),
-                new GreenDie(),
-                new GreenDie(),
-                new GreenDie(),
-                new YellowDie(),
-                new YellowDie(),
-                new YellowDie(),
-                new YellowDie(),
-                new RedDie(),
-                new RedDie(),
-                new RedDie(),
-            };
 
-            Random.Shared.Shuffle(CollectionsMarshal.AsSpan(dice));
+            List<Die> gameDice = new List<Die>();
+            ReloadGameDice(gameDice);
 
-            // Roll the dice and display the results
-            foreach (var die in dice)
+            Random.Shared.Shuffle(CollectionsMarshal.AsSpan(gameDice));
+
+            int player1Score = 0;
+            int player2Score = 0;
+            int currentPlayer = 1;
+
+            List<Die> playerCurrentDice = new List<Die>();
+            List<Die> playerCurrentShotguns = new List<Die>();
+            List<Die> playerCurrentBrains = new List<Die>();
+            bool keepGoing = true;
+
+            do
             {
-                die.Roll();
-                die.ShowDieResult();
-            }
+                int currentDiceCount = playerCurrentDice.Count;
+                for (int i = 0; i < 3 - currentDiceCount; i++)
+                {
+                    playerCurrentDice.Add(gameDice[i]);
+                    gameDice.RemoveAt(i);
+                }
+
+
+                foreach (var die in playerCurrentDice)
+                {
+                    die.Roll();
+                    die.ShowDieResult();
+
+                    if (die.GetSideUp() == DiceSide.Brain)
+                    {
+                        playerCurrentBrains.Add(die);
+                    }
+                    else if (die.GetSideUp() == DiceSide.Shotgun)
+                    {
+                        playerCurrentShotguns.Add(die);
+                    }
+                }
+
+
+                Console.WriteLine("Player " + currentPlayer + " Brains: " + playerCurrentBrains.Count + ", Shotgun blasts: " + playerCurrentShotguns.Count);
+                
+                if (playerCurrentShotguns.Count >= 3)
+                {
+                    Console.WriteLine("You have been shotgunned! You lose all your brains.");
+                    playerCurrentBrains.Clear();
+                    playerCurrentShotguns.Clear();
+                    playerCurrentDice.Clear();
+                    ReloadGameDice(gameDice);
+                    if(currentPlayer == 1)
+                    {
+                        currentPlayer = 2;
+                    }
+                    else
+                    {
+                        currentPlayer = 1;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Do you want to roll again? (y/n)");
+                    string input = Console.ReadLine();
+                    if (input.ToLower() != "y")
+                    {
+                        playerCurrentBrains.Clear();
+                        playerCurrentShotguns.Clear();
+                        playerCurrentDice.Clear();
+                        ReloadGameDice(gameDice);
+                        //keepGoing = false;
+                        if (currentPlayer == 1)
+                        {
+                            AddBrainsToPlayerScore(ref player1Score, playerCurrentBrains.Count);   //convert this to a numPlayers array
+                            currentPlayer = 2;
+                        }
+                        else
+                        {
+                            AddBrainsToPlayerScore(ref player2Score, playerCurrentBrains.Count);
+                            currentPlayer = 1;
+                        }
+                    }
+                }
+
+            } while (keepGoing && !(playerCurrentShotguns.Count >= 3));
         }
 
-        
+        public static void AddBrainsToPlayerScore(ref int playerScore, int brains)
+        {
+            playerScore += brains;
+        }
+
+        public static void ReloadGameDice(List<Die> dice)
+        {
+            dice.Clear();
+            dice.AddRange(new List<Die>
+            {
+                new GreenDie(), new GreenDie(), new GreenDie(), new GreenDie(), new GreenDie(), new GreenDie(), new YellowDie(), new YellowDie(), new YellowDie(), new YellowDie(), new RedDie(), new RedDie(), new RedDie(),
+            });
+        }
 
 
+
+
+        //Object classes to support the game.
 
         internal abstract class Die
         {
@@ -68,9 +142,14 @@ namespace ConsoleZombieDice
                 int rollIndex = rand.Next(0, 6);
                 SideUp = Sides[rollIndex];
             }
+
+            public DiceSide GetSideUp()
+            {
+                return SideUp;
+            }
+
             public abstract void ShowDieResult();
             public abstract DieColor GetDieColor();
-
         }
 
         internal class GreenDie : Die
@@ -87,8 +166,8 @@ namespace ConsoleZombieDice
                     DiceSide.Footsteps
                 };
             }
-            public override void ShowDieResult() 
-            { 
+            public override void ShowDieResult()
+            {
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine(SideUp);
                 Console.ResetColor();
